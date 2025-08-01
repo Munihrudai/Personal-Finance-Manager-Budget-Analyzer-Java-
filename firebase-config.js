d6f43684a53eb7c255226c7fa26601bc3f6d5ecd
Munihrudai <munihrudai0810@gmail.com>
2025-08-01 22:11:46 +0530

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2AdRz432OTS72gZNvAP3mupkzWNcWnAo",
+  authDomain: "financemanager-49119.firebaseapp.com",
+  projectId: "financemanager-49119",
+  storageBucket: "financemanager-49119.firebasestorage.app",
+  messagingSenderId: "337789373045",
+  appId: "1:337789373045:web:6771ea63cc3edb6b47063c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
